d970c7327e0c79237f1856ccec745a1d43c5d976
Zhang Ming <zmgithub@163.com>
2023-03-14 11:25:58 +0800

    增加一个标准枚举接口

---
+++ src/main/java/cn/zmdo/web/common/enumeration/StandardEnumerator.java
@@ -0,0 +1,18 @@
+package cn.zmdo.web.common.enumeration;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * 标准枚举接口
+ * @param <T> 枚举类
+ */
+public interface StandardEnumerator<T extends Enum<T>> {
+
+    int code();
+
+    @JsonValue
+    String value();
+
+    T valueOf(int code);
+
+}
